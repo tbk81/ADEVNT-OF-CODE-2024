cbabd7fc662b4343717f181deeb328faa2d3da53
tbk81 <dev@tbk81.com>
2024-12-02 11:47:26 -0800

    still on day 1

--- Day-1/main.go
+++ Day-1/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sort"
+	"os"
+	"strings"
 )
 
 func abs(x int) int {
@@ -13,26 +15,27 @@ func abs(x int) int {
 }
 
 func main() {
+	file, err := os.Open("/home/trevor/go-projects/ADVENT-OF-CODE-2024/Day-1/numbers.txt")
+	if err != nil {
+		panic(err)
+	}
+	// var ls1 []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		ls1, ls2, found := strings.Cut(line, " ")
+		fmt.Printf("%v\t%v\t%t\n", ls1, ls2, found)
+	}
 
-	ls1 := []int{3, 4, 2, 1, 3, 3}
-	// fmt.Println(ls1)
-	sort.Ints(ls1)
-	fmt.Println(ls1)
+	// ls1 := []int{3, 4, 2, 1, 3, 3}
+	// sort.Ints(ls1)
 
-	ls2 := []int{4, 3, 5, 3, 9, 3}
-	// fmt.Println(ls2)
-	sort.Ints(ls2)
-	fmt.Println(ls2)
+	// ls2 := []int{4, 3, 5, 3, 9, 3}
+	// sort.Ints(ls2)
 
-	// diff1 := abs(ls1[0] - ls2[0])
-	// fmt.Println(diff1)
-
-	// diff2 := abs(ls1[1] - ls2[1])
-	// fmt.Println(diff2)
-
-	// tot := diff1 + diff2
-	// fmt.Println(tot)
-
-	for i:=0;
-
+	// sum := 0
+	// for i := 0; i < len(ls1); i++ {
+	// 	sum += abs(ls1[i] - ls2[i])
+	// }
+	// fmt.Println(sum)
 }
