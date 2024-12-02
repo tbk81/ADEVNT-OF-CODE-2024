f18379270d0d916ddc94c5a94ae7e56c1230e737
tbk81 <dev@tbk81.com>
2024-12-02 14:19:38 -0800

    on to something

--- Day-1/main.go
+++ Day-1/main.go
@@ -21,19 +21,18 @@ func main() {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	lines := make([]string, 0)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	ls1, ls2, found := strings.Cut(lines, " ")
-	fmt.Printf("%v\t%v\t%t\n", ls1, ls2, found)
+	ls1 := make([]string, 0)
+	ls2 := make([]string, 0)
 
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	ls1, ls2, found := strings.Cut(line, " ")
-	// 	fmt.Printf("%v\t%v\t%t\n", ls1, ls2, found)
-	// }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		ls1 = append(ls1, parts[0])
+		ls2 = append(ls2, parts[1])
+	}
+
+	fmt.Println(ls1[0])
+	fmt.Println(ls2[0])
 
 	// ls1 := []int{3, 4, 2, 1, 3, 3}
 	// sort.Ints(ls1)
@@ -41,9 +40,9 @@ func main() {
 	// ls2 := []int{4, 3, 5, 3, 9, 3}
 	// sort.Ints(ls2)
 
-	// sum := 0
-	// for i := 0; i < len(ls1); i++ {
-	// 	sum += abs(ls1[i] - ls2[i])
-	// }
-	// fmt.Println(sum)
+	sum := 0
+	for i := 0; i < len(ls1); i++ {
+		sum += abs(ls1[i] - ls2[i])
+	}
+	fmt.Println(sum)
 }
