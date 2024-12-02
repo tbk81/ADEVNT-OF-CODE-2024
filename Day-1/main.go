7e5d22d18cc3899b3495959b6c80bb23bebd3904
tbk81 <dev@tbk81.com>
2024-12-02 05:58:22 -0800

    more day 1

--- Day-1/main.go
+++ Day-1/main.go
@@ -5,17 +5,15 @@ import (
 	"sort"
 )
 
-// var ls1 []int
-// var ls2 []int
-
-// ls1 = make([]int 3,4,2,1,3,3)
-
-// func sortInt(xi []int) (ordered []int) {
-// 	sort.Ints(xi)
-// 	return ordered
-// }
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 
 func main() {
+
 	ls1 := []int{3, 4, 2, 1, 3, 3}
 	// fmt.Println(ls1)
 	sort.Ints(ls1)
@@ -26,23 +24,15 @@ func main() {
 	sort.Ints(ls2)
 	fmt.Println(ls2)
 
-	diff1 := ls1[0] - ls2[0]
-	if diff1 < 0 {
-		diff1 = -diff1
-	}
-	fmt.Println(diff1)
+	// diff1 := abs(ls1[0] - ls2[0])
+	// fmt.Println(diff1)
 
-	diff2 := ls1[1] - ls2[1]
-	if diff2 < 0 {
-		diff2 = -diff2
-	}
-	fmt.Println(diff2)
+	// diff2 := abs(ls1[1] - ls2[1])
+	// fmt.Println(diff2)
 
-	tot := diff1 + diff2
-	fmt.Println(tot)
+	// tot := diff1 + diff2
+	// fmt.Println(tot)
 
-	// for _, num1 := range ls1 {
-	// 	fmt.Println(num1)
-	// }
+	for i:=0;
 
 }
